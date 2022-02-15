77fe7759ec8c3451d5c322aef3f782f2b9ae7b3a
Abidur Rahman <abid43iiuc@gmail.com>
2022-02-15 18:40:46 +0600

    insertAdjacentHTML() method

---
+++ insertAdjacentHtml/app.js
@@ -0,0 +1,7 @@
+let list = document.querySelector('#list');
+
+list.insertAdjacentHTML('beforebegin', '<h2>Web Technology</h2>');
+list.insertAdjacentHTML('afterbegin', '<li>HTML</li>');
+list.insertAdjacentHTML('beforeend', '<li>Javascript</li>');
+list.insertAdjacentHTML('afterend', '<p>For frontend developers</p>');
+
